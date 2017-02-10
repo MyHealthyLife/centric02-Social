e41fb7367954540bde668812f3873d43e876a569
StefanoTavonatti93 <stefano.tavonatti@studenti.unitn.it>
2017-02-10 17:36:33 +0100

    put recipe

--- centric02-social/src/myhealthylife/centric2/rest/RecipesHandler.java
+++ centric02-social/src/myhealthylife/centric2/rest/RecipesHandler.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -12,6 +13,7 @@ import javax.ws.rs.core.Response;
 
 import myhealthylife.centric2.rest.model.Recipe;
 import myhealthylife.centric2.rest.model.RecipeList;
+import myhealthylife.centric2.util.Utilities;
 
 @Path("/recipe")
 public class RecipesHandler {
@@ -41,6 +43,28 @@ public class RecipesHandler {
 		r=Recipe.saveRecipe(r);
 		r.computeFoods();
 		return r;
+	}
+	
+	@PUT
+	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	public Response updateRecipe(Recipe r){
+		Recipe stored=Recipe.getRecipeById(r.getRecipeId());
+		
+		if(stored==null)
+			return Utilities.throwResourceNotFound();
+		
+		r.computeIDS();
+		
+		stored.setDescription(r.getDescription());
+		stored.setIngredientsIDs(r.getIngredientsIDs());
+		stored.setName(r.getName());
+		
+		stored=Recipe.updateRecipe(stored);
+		stored.computeFoods();
+		
+		return Utilities.throwOK(stored);
+		
 	}
 
 }
