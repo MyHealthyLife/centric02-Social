559c99705f32c9343eeff8f4a1307b747d5c4ee5
StefanoTavonatti93 <stefano.tavonatti@studenti.unitn.it>
2017-02-10 17:28:19 +0100

    get recipe list and post recipe

--- centric02-social/src/myhealthylife/centric2/rest/RecipesHandler.java
+++ centric02-social/src/myhealthylife/centric2/rest/RecipesHandler.java
@@ -1,10 +1,14 @@
 package myhealthylife.centric2.rest;
 
+import java.util.Iterator;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import myhealthylife.centric2.rest.model.Recipe;
 import myhealthylife.centric2.rest.model.RecipeList;
@@ -17,10 +21,26 @@ public class RecipesHandler {
     @Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
 	public RecipeList hello(){
 		RecipeList rl=new RecipeList();
-		
 		rl.setRecipes(Recipe.getAll());
+		Iterator<Recipe> it=rl.getRecipes().iterator();
+		
+		while(it.hasNext()){
+			it.next().computeFoods();
+		}
+		
+		
 		
 		return rl;
+	}
+	
+	@POST
+	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	public Recipe saveRecipe(Recipe r){
+		r.computeIDS();
+		r=Recipe.saveRecipe(r);
+		r.computeFoods();
+		return r;
 	}
 
 }
--- centric02-social/src/myhealthylife/centric2/rest/model/Recipe.java
+++ centric02-social/src/myhealthylife/centric2/rest/model/Recipe.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -56,6 +57,11 @@ public class Recipe {
 	public List<Long> getIngredientsIDs() {
 		
 		/*only the ids will be saved in the DB, calculate the id when someone asking for it*/
+		//computeIDS();
+		return ingredientsIDs;
+	}
+
+	public void computeIDS() {
 		ingredientsIDs=new ArrayList<Long>();
 		
 		if(ingredients!=null){
@@ -67,7 +73,6 @@ public class Recipe {
 				ingredientsIDs.add(f.getIdFood());
 			}
 		}
-		return ingredientsIDs;
 	}
 
 	public void setIngredientsIDs(List<Long> ingredientsIDs) {
@@ -103,11 +108,16 @@ public class Recipe {
 		/* when someone asking for the ingredients list centric02 retrieves the list 
 		 * the food date from the service03 based on the ids */
 		
-		if(ingredients==null)
-			ingredients=new ArrayList<Food>();
+		//computeFoods();
 		
-		if(getIngredientsIDs()==null)
-			return ingredients;
+		return ingredients;
+	}
+
+	public void computeFoods() {
+		ingredients=new ArrayList<Food>();
+		
+		if(ingredientsIDs==null)
+			return;
 		
 		Foods fs=ServicesLocator.getFoodServiceConnection();
 		
@@ -122,11 +132,36 @@ public class Recipe {
 			
 			ingredients.add(f);
 		}
-		
-		return ingredients;
 	}
 
 	public void setIngredients(List<Food> ingredients) {
 		this.ingredients = ingredients;
 	}
+	
+	public static Recipe getRecipeById(long id) {
+        EntityManager em = CentricServiceDao.instance.createEntityManager();
+        Recipe p = em.find(Recipe.class, id);
+        CentricServiceDao.instance.closeConnections(em);
+        return p;
+    }
+	
+	public static Recipe saveRecipe(Recipe p) {
+        EntityManager em = CentricServiceDao.instance.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.persist(p);
+        tx.commit();
+        CentricServiceDao.instance.closeConnections(em);
+        return p;
+    } 
+
+    public static Recipe updateRecipe(Recipe p) {
+        EntityManager em = CentricServiceDao.instance.createEntityManager(); 
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        p=em.merge(p);
+        tx.commit();
+        CentricServiceDao.instance.closeConnections(em);
+        return p;
+    }
 }
