28d6f31c69167d4e14649bc1a66d702837d9d8a4
SimoneScalco93 <simone.scalco@studenti.unitn.it>
2017-02-11 16:20:30 +0100

    Added the possibility to create a food

--- centric02-social/src/myhealthylife/centric2/rest/FoodHandler.java
+++ centric02-social/src/myhealthylife/centric2/rest/FoodHandler.java
@@ -2,15 +2,18 @@ package myhealthylife.centric2.rest;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.ws.Holder;
 
 import myhealthylife.centric2.util.ServicesLocator;
 import myhealthylife.centric2.util.Utilities;
+import myhealthylife.nutritionservice.soap.Food;
 import myhealthylife.nutritionservice.soap.FoodList;
 import myhealthylife.nutritionservice.soap.Foods;
 
@@ -47,6 +50,31 @@ public class FoodHandler {
         
         // Returns the list of foods
 		return Utilities.throwOK(foodListToReturn);
+		
+	}
+	
+	
+	@Path("/")
+	@POST
+	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	public Response createFood(Food foodToSave){
+
+		Foods fs = ServicesLocator.getFoodServiceConnection();
+		
+		Food newFood = new Food();
+		
+		// Tries to save the food passed in input
+        if(foodToSave.getName()!=null) {
+        	
+        	Holder<Food> foodHolder = new Holder<Food>(foodToSave);
+            fs.createFood(foodHolder);
+            newFood = foodHolder.value;
+        	
+        }
+        
+        // Return the new food saved in the database
+		return Utilities.throwOK(newFood);
 		
 	}
 	
